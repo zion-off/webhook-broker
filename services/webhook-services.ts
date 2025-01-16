bc41a57e69e886e420dcc887dbf17d91efc2f705
Nahid <Abu.Siddik@optimizely.com>
2025-01-16 20:21:34 +0600

    renames event_name to eventName & webhook_url to webhookUrl

--- services/webhook-services.ts
+++ services/webhook-services.ts
@@ -1,24 +1,19 @@
 import { redis } from "..";
 import { HttpError } from "@/utils/error";
 
-export async function registerWebhookService(
-  event_name: string,
-  url: string
-) {
+export async function registerWebhookService(eventName: string, url: string) {
   try {
-    await redis.sAdd(`webhooks:${event_name}`, url);
+    await redis.sAdd(eventName, url);
   } catch (error) {
     throw new HttpError("Failed to register webhook in Redis", 500);
   }
 }
 
-export async function getWebhookByEventNameService(
-  event_name: string
-) {
+export async function getWebhookByEventNameService(eventName: string) {
   try {
-    const urls = await redis.sMembers(`webhooks:${event_name}`);
+    const urls = await redis.sMembers(eventName);
     return {
-      eventName: event_name,
+      eventName: eventName,
       webhookUrls: urls,
     };
   } catch (error) {
